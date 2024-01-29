6f21de6b29d74ae23ffcc7b3399699028297cd3a
Diego Govea <87109033+diegogovea@users.noreply.github.com>
2024-01-28 19:27:06 -0600

    Update ejemplo1Real.py
    Hice que los botones se visualizaran en la parte de abajo, en lugar de tener un único contenedor (button_frame) para todos los botones, creé dos contenedores: uno para las barras de progreso y otro para los botones.

--- Actividad 3 - Procesamiento Por Lotes/ejemplo1Real.py
+++ Actividad 3 - Procesamiento Por Lotes/ejemplo1Real.py
@@ -4,36 +4,40 @@ import datetime
 from tkinter import *
 from tkinter import ttk
 
-window=Tk()
+window = Tk()
 window.title('Multiprocess and lote')
 window.geometry("600x400")
 
 
 def lotes():
-    #print
     for x in range(50):
-        if(my_prSong['value']<=98):
-            my_prSong['value']+=4
-            PorcentAudio.config( text=my_prSong['value'])
+        if my_prSong['value'] <= 98:
+            my_prSong['value'] += 4
+            PorcentAudio.config(text=my_prSong['value'])
             window.update_idletasks()
-            time.sleep(0.01)#pausas de 1ms
+            time.sleep(0.01)
+
 
 def multi():
     t3.start()
+
+
 def song():
     for x in range(100):
-        if(my_prSong['value']<=99):
-                my_prSong['value']+=x
-                PorcentAudio.config( text=my_prSong['value'])
-                window.update_idletasks()
-                time.sleep(0.02)
+        if my_prSong['value'] <= 99:
+            my_prSong['value'] += x
+            PorcentAudio.config(text=my_prSong['value'])
+            window.update_idletasks()
+            time.sleep(0.02)
     return
+
 
 def stop():
     my_prSong.stop()
-    PorcentAudio.config( text="")
+    PorcentAudio.config(text="")
 
-lbSpace= Label(window, text=" ")
+
+lbSpace = Label(window, text=" ")
 lbSpace.pack(pady=20)
 
 #creacion de hilos
@@ -41,29 +45,29 @@ tiempo=datetime.datetime.now()
 
 t3=threading.Thread(name="hilo_3", target=song)
 
+# Contenedor para las barras de progreso
+progress_frame = Frame(window)
+progress_frame.pack()
+
+# AUDIO
+lbCancion = Label(progress_frame, text="Deleting song")
+lbCancion.pack(pady=5)
+my_prSong = ttk.Progressbar(progress_frame, orient=HORIZONTAL, length=300, mode='determinate')
+my_prSong.pack()
+
+PorcentAudio = Label(progress_frame, text=" ")
+PorcentAudio.pack(pady=10)
+
+# Contenedor para los botones
 button_frame = Frame(window)
 button_frame.pack()
 
-#AUDIO
-
-lbCancion= Label(window, text="Deleting song")
-lbCancion.pack(pady=5)
-my_prSong = ttk.Progressbar(window, orient=HORIZONTAL,length=300, mode='determinate')
-my_prSong.pack()
-
-
-
-PorcentAudio= Label(window, text=" ")
-PorcentAudio.pack(pady=10)
-
-
-
-#BOTONES PARA EJECUTAR PROCESOS
-my_button=Button(button_frame, text="RESET", command=stop)
+# BOTONES PARA EJECUTAR PROCESOS
+my_button = Button(button_frame, text="RESET", command=stop)
 my_button.pack(side=LEFT, padx=20)
-multibtn=Button(button_frame, text="MULTIPROCES", command=multi)
+multibtn = Button(button_frame, text="MULTIPROCES", command=multi)
 multibtn.pack(side=LEFT, padx=5)
-lotesbtn=Button(button_frame, text="LOTES", command=lotes)
+lotesbtn = Button(button_frame, text="LOTES", command=lotes)
 lotesbtn.pack(side=LEFT, padx=5)
 
 window.mainloop()
